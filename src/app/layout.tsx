32448ae539f9ea9781507dc5c4f7f4945b3db15e
po4ti_ka3ax <dmtr.povush@gmail.com>
2025-05-29 00:11:51 +0500

    add design to profile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
         <SessionProvider session={session}>
           <div className="bg-[#1A1A1A] min-h-screen text-[white] py-[20px]">
             <Header/>
-            <div className="mx-auto max-w-[1600px] px-5 ">
+            <div className="mx-auto max-w-[1400px] px-5 ">
               {children}
             </div>
           </div>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -35,23 +35,25 @@ const Profile = () => {
         <>
             <ProtectedMiddleware>
                 <h1 className="text-center text-[24px]">Profile</h1>
-                <div className="bg-[#3E3E3E] max-w-[500px] mt-[100px] m-auto px-[20px] py-[40px] rounded-[10px] flex">
+                <div className="bg-[#3E3E3E] max-w-[500px] mt-[100px] m-auto px-[20px] py-[40px] rounded-[10px] flex justify-around items-center">
                     <div className="">
                         <p>Username: {userInfo.username}</p>
                         <p>Email: {userInfo.email}</p>
                         <p>Role: {userInfo.role?.slug}</p>
                     </div>
-                    <div className="">
-                        <Avatar>
-                            <AvatarImage src="" />
-                            <AvatarFallback>{userInfo.username?.slice(0, 2).toUpperCase()}</AvatarFallback>
-                        </Avatar>
-                    </div>
-                    <div className="">
-                        <button onClick={() => setEdit(!edit)}>Edit profile info</button>
+                    <div className="flex flex-col justify-center items-center">
+                            <Avatar className="w-[90px] h-[90px] mb-[10px]">
+                                <AvatarImage src="" />
+                                <AvatarFallback>{userInfo.username?.slice(0, 2)}</AvatarFallback>
+                            </Avatar>
+                            <button className="px-[10px] py-[10px] bg-[#FFCC70] text-black rounded-[10px]" onClick={() => setEdit(!edit)}>Edit profile info</button>
                     </div>
                 </div>
-                <button onClick={() => handleSignOut()}>Sign out</button>
+
+                <div className="text-center mt-[30px]">
+                    <button className="text-center cursor-pointer text-[#C82323] border-[#C82323] hover:bg-[#C82323] hover:text-[#ffffff] duration-300 border-1 rounded-[10px] px-[10px] py-[10px]" onClick={() => handleSignOut()}>Sign out</button>
+                </div>
+
             </ProtectedMiddleware>
         </>
     );
